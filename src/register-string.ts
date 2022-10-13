91702036721a53ecc619c195e15af7af357aabee
val.istar.guo <val.istar.guo@gmail.com>
2022-10-13 17:56:38 +0800

    fix: r__replaced should be named r__replace

--- src/register-string.ts
+++ src/register-string.ts
@@ -5,7 +5,7 @@ import { wrap } from './wrap'
 
 
 export function registerString(hbs: typeof Handlebars): void {
-  hbs.registerHelper('r__replaced', wrap(R.replace))
+  hbs.registerHelper('r__replace', wrap(R.replace))
   hbs.registerHelper('r__split', wrap(R.split))
   hbs.registerHelper('r__toLower', blockWrap(R.toLower))
   hbs.registerHelper('r__toUpper', blockWrap(R.toUpper))
